70f06196eb5635a2ff62a93698922ad8a50996ed
OsborneSebuabe <102943605+OsborneSebuabe@users.noreply.github.com>
2022-04-15 19:43:00 +0000

    Add files via upload

---
+++ CalculateAreaOfACicle.java
@@ -0,0 +1,14 @@
+
+public class CalculateAreaOfACicle {
+	
+	public static void main(String[] args) {
+
+	int radius = 3;
+	System.out.println("The radius of the circle is" + radius);
+	
+	double area = 3.1428 * radius * radius;
+	
+	System.out.println("Area of a circle is" + area);
+
+	}
+}
